455dde265ac0859be1123870dc2e21c0b666e21d
Moritz Heinemann <mo@blog-srv.net>
2018-07-31 17:39:04 +0200

    add InputLayer for beat

---
+++ Os2lPlugin/Os2lInputLayer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using org.dmxc.lumos.Kernel.Beat;
+using org.dmxc.lumos.Kernel.Input;
+
+namespace Os2lPlugin
+{
+    public class Os2lInputLayer : AbstractKernelInputLayer, IDisposable
+    {
+        private BeatChannel _beat;
+
+        public Os2lInputLayer()
+            : base(new InputID("{dc94cd84-66ac-474c-8e06-1202a604692a}", InputLayerManager.getInstance().SessionName), "OS2L")
+        {
+            _beat = new BeatChannel("{ea70b486-544a-454b-941a-475c9cdd793a}", this);
+            _beat.Name = "OS2L Beat";
+            AddInputChannel(_beat);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
--- Os2lPlugin/Os2lPlugin.cs
+++ Os2lPlugin/Os2lPlugin.cs
@@ -1,4 +1,5 @@
 ﻿using LumosLIB.Kernel.Log;
+using org.dmxc.lumos.Kernel.Input;
 using org.dmxc.lumos.Kernel.Plugin;
 
 namespace Os2lPlugin
@@ -8,6 +9,8 @@ namespace Os2lPlugin
         private const string PLUGIN_ID = "{b43ef54b-0124-413a-b0eb-763fc936be4a}";
 
         private static readonly ILumosLog log = LumosLogger.getInstance<Os2lPlugin>();
+
+        private Os2lInputLayer _inputLayer;
 
         public Os2lPlugin() : base(PLUGIN_ID, "OS2L Plugin")
         {
@@ -22,11 +25,18 @@ namespace Os2lPlugin
         protected override void startupPlugin()
         {
             log.Info("startupPlugin()");
+
+            _inputLayer = new Os2lInputLayer();
+            InputLayerManager.getInstance().registerInputLayer(_inputLayer);
         }
 
         protected override void shutdownPlugin()
         {
             log.Info("shutdownPlugin()");
+
+            InputLayerManager.getInstance().deregisterInputLayer(_inputLayer);
+            _inputLayer.Dispose();
+            _inputLayer = null;
         }
     }
 }
