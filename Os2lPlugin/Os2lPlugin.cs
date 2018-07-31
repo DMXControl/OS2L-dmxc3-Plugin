c2d347e3d1070b642fd2648946ea661111f568c1
Moritz Heinemann <mo@blog-srv.net>
2018-07-31 13:01:15 +0200

    add Lumos references, use KernelPluginBase

--- Os2lPlugin/Os2lPlugin.cs
+++ Os2lPlugin/Os2lPlugin.cs
@@ -1,6 +1,32 @@
-﻿namespace Os2lPlugin
+﻿using LumosLIB.Kernel.Log;
+using org.dmxc.lumos.Kernel.Plugin;
+
+namespace Os2lPlugin
 {
-    public class Os2lPlugin
+    public class Os2lPlugin : KernelPluginBase
     {
+        private const string PLUGIN_ID = "{b43ef54b-0124-413a-b0eb-763fc936be4a}";
+
+        private static readonly ILumosLog log = LumosLogger.getInstance<Os2lPlugin>();
+
+        public Os2lPlugin() : base(PLUGIN_ID, "OS2L Plugin")
+        {
+            log.Info("Os2lPlugin()");
+        }
+
+        protected override void initializePlugin()
+        {
+            log.Info("initializePlugin()");
+        }
+
+        protected override void startupPlugin()
+        {
+            log.Info("startupPlugin()");
+        }
+
+        protected override void shutdownPlugin()
+        {
+            log.Info("shutdownPlugin()");
+        }
     }
 }
