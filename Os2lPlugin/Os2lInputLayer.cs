9632891345660fc2411226070b473aaf34801987
Moritz Heinemann <mo@blog-srv.net>
2018-08-19 20:41:41 +0200

    remove unused import

--- Os2lPlugin/Os2lInputLayer.cs
+++ Os2lPlugin/Os2lInputLayer.cs
@@ -2,7 +2,6 @@
 using System.Diagnostics;
 using System.Net;
 using System.Net.Sockets;
-using System.Runtime.InteropServices;
 using System.Threading;
 using LumosLIB.Kernel.Log;
 using Mono.Zeroconf;
