fc49cc0da3254fcf5c6ef0ccbe8ee9746072f0ee
Moritz Heinemann <mo@blog-srv.net>
2019-11-01 12:19:28 +0100

    move OS2L server to plugin class

--- Os2lPlugin/Os2lInputSource.cs
+++ Os2lPlugin/Os2lInputSource.cs
@@ -1,133 +1,25 @@
-﻿using System;
-using System.Diagnostics;
-using System.Net;
-using System.Net.Sockets;
-using System.Threading;
-using LumosLIB.Kernel;
-using LumosLIB.Kernel.Log;
-using Mono.Zeroconf;
-using Newtonsoft.Json.Linq;
+﻿using LumosLIB.Kernel;
 using org.dmxc.lumos.Kernel.Input.v2;
-using org.dmxc.lumos.Kernel.Log;
 
 namespace Os2lPlugin
 {
-    public class Os2lInputSource : AbstractInputSource, IDisposable
+    public class Os2lInputSource : AbstractInputSource
     {
-        private const int OS2L_PORT_MIN = 8010;
-        private const int OS2L_PORT_MAX = 8060;
-
-        private static readonly ILumosLog log = LumosLogger.getInstance<Os2lInputSource>();
-
-        private bool _shutdown;
-        private TcpListener _server;
-        private Thread _thread;
-        private RegisterService _zeroconfService;
-
         public Os2lInputSource()
             : base("{dc94cd84-66ac-474c-8e06-1202a604692a}", "Beat", new ParameterCategory("OS2L"))
         {
-            _shutdown = false;
-
-            // find unused port between OS2L_PORT_MIN and OS2L_PORT_MAX
-            int port = OS2L_PORT_MIN;
-            bool started = false;
-            while (!started && port <= OS2L_PORT_MAX)
-            {
-                try
-                {
-                    _server = new TcpListener(IPAddress.Any, port);
-                    _server.Start();
-                    started = true;
-                    log.Info("OS2L Server listens on port {0}", port);
-                }
-                catch (Exception e)
-                {
-                    log.Info("Failed to listen on Port {0}", port);
-                    port++;
-                }
-            }
-
-            if (port > OS2L_PORT_MAX)
-            {
-                log.Warn("OS2L Server start failed!");
-                return;
-            }
-
-            _thread = new Thread(Listen);
-            _thread.Start();
-
-            try
-            {
-                _zeroconfService = new RegisterService();
-                // Do not set Name option, default name is hostname.
-                _zeroconfService.RegType = "_os2l._tcp";
-                _zeroconfService.Port = (short) port;
-                _zeroconfService.Register();
-            }
-            catch (Exception e)
-            {
-                log.Warn("Bonjour init failed!");
-                KernelLogManager
-                    .getInstance()
-                    .sendUserNotification("OS2L Service could not be initialized!\n" +
-                                          "Please make sure that Bonjour is installed:\n" +
-                                          "https://support.apple.com/kb/DL999",
-                                          EventLogEntryType.Warning);
-            }
         }
 
-        private void Listen()
+        public void IncrementBeat()
         {
-            try
+            if (!(this.CurrentValue is ulong))
             {
-                Byte[] bytes = new Byte[1024];
-                String data = null;
-
-                while (!_shutdown)
-                {
-                    TcpClient client = _server.AcceptTcpClient();
-                    NetworkStream stream = client.GetStream();
-
-                    int i;
-                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
-                    {
-                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
-                        JObject obj = JObject.Parse(data);
-                        String evt = obj["evt"].Value<String>();
-                        if (evt == "beat")
-                        {
-                            if (!(this.CurrentValue is ulong))
-                            {
-                                this.CurrentValue = (ulong) 1;
-                            }
-                            else
-                            {
-                                this.CurrentValue = (ulong) this.CurrentValue + 1;
-                            }
-                        }
-
-                        // TODO other events and other beat properties
-                        //Console.WriteLine("Received: {0}", data);
-                    }
-
-                    client.Close();
-                }
+                this.CurrentValue = (ulong) 1;
             }
-            catch (Exception e)
+            else
             {
-                // _server.AcceptTcpClient() is a blocking call and throws an exception on _server.Stop()
+                this.CurrentValue = (ulong) this.CurrentValue + 1;
             }
-        }
-
-        protected override void DisposeHook(bool disposing)
-        {
-            base.DisposeHook(disposing);
-
-            _shutdown = true;
-            _zeroconfService?.Dispose();
-            _server?.Stop();
-            _thread?.Join();
         }
 
         public override EWellKnownInputType AutoGraphIOType => EWellKnownInputType.BEAT;
--- Os2lPlugin/Os2lPlugin.cs
+++ Os2lPlugin/Os2lPlugin.cs
@@ -1,5 +1,13 @@
-﻿using LumosLIB.Kernel.Log;
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using LumosLIB.Kernel.Log;
+using Mono.Zeroconf;
+using Newtonsoft.Json.Linq;
 using org.dmxc.lumos.Kernel.Input.v2;
+using org.dmxc.lumos.Kernel.Log;
 using org.dmxc.lumos.Kernel.Plugin;
 
 namespace Os2lPlugin
@@ -7,10 +15,17 @@ namespace Os2lPlugin
     public class Os2lPlugin : KernelPluginBase
     {
         private const string PLUGIN_ID = "{b43ef54b-0124-413a-b0eb-763fc936be4a}";
+        private const int OS2L_PORT_MIN = 8010;
+        private const int OS2L_PORT_MAX = 8060;
 
         private static readonly ILumosLog log = LumosLogger.getInstance<Os2lPlugin>();
 
         private Os2lInputSource _inputSource;
+
+        private bool _shutdown;
+        private TcpListener _server;
+        private Thread _thread;
+        private RegisterService _zeroconfService;
 
         public Os2lPlugin() : base(PLUGIN_ID, "OS2L Plugin")
         {
@@ -25,16 +40,106 @@ namespace Os2lPlugin
             log.Debug("Register Os2lInputSource");
             _inputSource = new Os2lInputSource();
             InputManager.getInstance().RegisterSource(_inputSource);
+
+            _shutdown = false;
+
+            // find unused port between OS2L_PORT_MIN and OS2L_PORT_MAX
+            int port = OS2L_PORT_MIN;
+            bool started = false;
+            while (!started && port <= OS2L_PORT_MAX)
+            {
+                try
+                {
+                    _server = new TcpListener(IPAddress.Any, port);
+                    _server.Start();
+                    started = true;
+                    log.Info("OS2L Server listens on port {0}", port);
+                }
+                catch (Exception e)
+                {
+                    log.Info("Failed to listen on Port {0}", port);
+                    port++;
+                }
+            }
+
+            if (port > OS2L_PORT_MAX)
+            {
+                log.Warn("OS2L Server start failed!");
+                return;
+            }
+
+            _thread = new Thread(Listen);
+            _thread.Start();
+
+            try
+            {
+                _zeroconfService = new RegisterService();
+                // Do not set Name option, default name is hostname.
+                _zeroconfService.RegType = "_os2l._tcp";
+                _zeroconfService.Port = (short) port;
+                _zeroconfService.Register();
+            }
+            catch (Exception e)
+            {
+                log.Warn("Bonjour init failed!");
+                KernelLogManager
+                    .getInstance()
+                    .sendUserNotification("OS2L Service could not be initialized!\n" +
+                                          "Please make sure that Bonjour is installed:\n" +
+                                          "https://support.apple.com/kb/DL999",
+                                          EventLogEntryType.Warning);
+            }
         }
 
         protected override void shutdownPlugin()
         {
+            _shutdown = true;
+            _zeroconfService?.Dispose();
+            _server?.Stop();
+            _thread?.Join();
+
             if (_inputSource != null)
             {
                 log.Debug("Unregister Os2lInputSource");
                 InputManager.getInstance().UnregisterSource(_inputSource);
                 _inputSource.Dispose();
                 _inputSource = null;
+            }
+        }
+
+        private void Listen()
+        {
+            try
+            {
+                Byte[] bytes = new Byte[1024];
+                String data = null;
+
+                while (!_shutdown)
+                {
+                    TcpClient client = _server.AcceptTcpClient();
+                    NetworkStream stream = client.GetStream();
+
+                    int i;
+                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                        JObject obj = JObject.Parse(data);
+                        String evt = obj["evt"].Value<String>();
+                        if (evt == "beat")
+                        {
+                            _inputSource.IncrementBeat();
+                        }
+
+                        // TODO other events and other beat properties
+                        //Console.WriteLine("Received: {0}", data);
+                    }
+
+                    client.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                // _server.AcceptTcpClient() is a blocking call and throws an exception on _server.Stop()
             }
         }
     }
